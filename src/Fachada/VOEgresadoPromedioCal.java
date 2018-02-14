e10352641c90ad0f0d9556b1053a0ff605feb71b
Tikongo <Tikongo@users.noreply.github.com>
2018-02-14 14:11:48 -0300

    modificacion VOegresado
    modificacion VOegresado

--- src/Fachada/VOEgresadoPromedioCal.java
+++ src/Fachada/VOEgresadoPromedioCal.java
@@ -2,21 +2,28 @@ package Fachada;
 
 public class VOEgresadoPromedioCal extends VOAlumno {
 
-	private Float promedioCal;
+	private Float promedioTotal;
+	private Float promedioAprob;
 	
-	public VOEgresadoPromedioCal(Long cedula, String nombre, String apellido, Float promedioCal) {
+	public VOEgresadoPromedioCal(Long cedula, String nombre, String apellido, Float pt, Float pa) {
 		super(cedula,nombre,apellido);
-		this.promedioCal = promedioCal;
+		this.promedioTotal = pt;
+		this.promedioAprob = pa;
 	}
 
 	//Getters
-	public Float getPromedioCal() {
-		return promedioCal;
+	public Float getPromedioTotal() {
+		return promedioTotal;
+	}
+	
+	public Float getPromedioAprob() {
+		return promedioAprob;
 	}
 
 	@Override
 	public String toString() {
-		return "VOEgresadoPromedioCal [promedioCal=" + promedioCal + ", toString()=" + super.toString() + "]";
+		return "VOEgresadoPromedioCal [promedioTotal=" + promedioTotal + ", promedioAprobadas=" + promedioAprob + 
+				" , toString()=" + super.toString() + "]";
 	}
 	
 	
