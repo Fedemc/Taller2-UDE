63e295a379e7a2c282178720ec397c30a042e318
Tikongo <Tikongo@users.noreply.github.com>
2018-02-15 12:15:20 -0300

    cambio nombre arbol, metodo público iterador
    cambio nombre arbol, metodo público iterador

--- src/Fachada/Alumnos.java
+++ src/Fachada/Alumnos.java
@@ -10,31 +10,37 @@ import Fachada.VOAlumno;
 
 public class Alumnos {
 	
-	private TreeMap<Long,Alumno> alumnos;
+	private TreeMap<Long,Alumno> arbol;
 	
 	//Constructor
 	public Alumnos() {
-		alumnos = new TreeMap<Long,Alumno>();
+		arbol = new TreeMap<Long,Alumno>();
 	}
 		
 	public boolean member(Long clave) {
-		return alumnos.containsKey(clave);
+		return arbol.containsKey(clave);
 	}
 	
 	public void insert(Alumno a) {
-		alumnos.put(a.getCedula(),a);
+		arbol.put(a.getCedula(),a);
 	}
 	
 	public Alumno find(Long clave) {
-		return alumnos.get(clave);
+		return arbol.get(clave);
+	}
+	
+	//Crear Iterador
+	public Iterator crearIterador() {
+		Iterator it = arbol.keySet().iterator();
+		return it;
 	}
 	
 	public List<VOAlumno> ListadoAlumnosApe(String ape) {
-		Iterator it = alumnos.keySet().iterator();	//Me genero un iterador con el set de cedulas del treemap
+		Iterator it = arbol.keySet().iterator();	//Me genero un iterador con el set de cedulas del treemap
 		List<VOAlumno> vOAlumnos = new ArrayList<>();
 		while (it.hasNext()) {
 			Long clave = (Long) it.next();	//El iterador solo tiene cedulas, casteo como long para traerme las cedulas
-			Alumno tempAlu = alumnos.get(clave);	//me genero un alumno temporal para almacenar los datos del que estoy leyendo con el iterador
+			Alumno tempAlu = arbol.get(clave);	//me genero un alumno temporal para almacenar los datos del que estoy leyendo con el iterador
 			if (tempAlu.getApellido().startsWith(ape)) {	//comparo si el apellido del alumno empieza con el string que se ingresó por parametro
 				//Convertir a VOAlumno y almacenar
 				VOAlumno voa = new VOAlumno(tempAlu.getCedula(),tempAlu.getNombre(),tempAlu.getApellido());
@@ -45,14 +51,14 @@ public class Alumnos {
 	}
 	
 	public VOAlumnoDetallado ListadoAlumnoCedulaCom(Long clave) {
-		Alumno tempAlu = (Alumno) alumnos.get(clave);
+		Alumno tempAlu = (Alumno) arbol.get(clave);
 		VOAlumnoDetallado voad = new VOAlumnoDetallado(tempAlu.getCedula(),tempAlu.getNombre(),tempAlu.getApellido(),tempAlu.getDomicilio(),
 					tempAlu.getTelefono(),tempAlu.getEmail(),tempAlu.calcularCuotaAlumno());
 		return voad;
 	}
 	
 	public VOBecadoDetallado ListadoAlumnoCedulaBec(Long clave) {
-		Becado tempBec = (Becado) alumnos.get(clave);
+		Becado tempBec = (Becado) arbol.get(clave);
 		VOBecadoDetallado vobd = new VOBecadoDetallado(tempBec.getCedula(),tempBec.getNombre(),tempBec.getApellido(),tempBec.getDomicilio(),
 					tempBec.getTelefono(),tempBec.getEmail(),tempBec.calcularCuotaAlumno(),tempBec.getPorcentaje(), tempBec.getDescripcionBeca());
 		return vobd;
--- src/Fachada/principal.java
+++ src/Fachada/principal.java
@@ -9,20 +9,22 @@ public class principal {
 	public static void main(String[] args) {
 		
 		
-		TreeMap<Long,Alumno> a = new TreeMap<Long,Alumno>();
+		/*TreeMap<Long,Alumno> a = new TreeMap<Long,Alumno>();*/
 		
+		Alumnos a = new Alumnos();
 		
 		Alumno a1 =  new Alumno(1,"andres","zubi","br",99,"sss@");
 		Alumno a2 =  new Alumno(5,"car","cha","sr",88,"hooo@");
 		Alumno a3 =  new Alumno(2,"fed","kol","pp",66,"ppp@");
-		a.put(a1.getCedula(), a1);
-		a.put(a2.getCedula(), a2);
-		a.put(a3.getCedula(), a3);
+		a.insert(a1);
+		a.insert(a2);
+		a.insert(a3);
 		
-		Iterator iter = a.keySet().iterator();
+		Iterator iter = a.crearIterador();
+		
 		while(iter.hasNext()) {
 			long ced = (long) iter.next();
-			VOAlumno voa = new VOAlumno(a.get(ced).getCedula(), a.get(ced).getNombre(), a.get(ced).getApellido());
+			VOAlumno voa = new VOAlumno(a.find(ced).getCedula(), a.find(ced).getNombre(), a.find(ced).getApellido());
 			System.out.println(voa.toString());
 		}
 		
