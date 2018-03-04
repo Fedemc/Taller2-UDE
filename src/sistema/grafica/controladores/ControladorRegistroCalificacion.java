6507d79e6c5e1673d5f7848ed17e8bfcd4ecd53a
Fede <fedemc@gmail.com>
2018-03-04 15:16:32 -0300

    Agregado controlador de registro de calificacion

---
+++ src/sistema/grafica/controladores/ControladorRegistroCalificacion.java
@@ -0,0 +1,78 @@
+package sistema.grafica.controladores;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Properties;
+
+import sistema.excepciones.AlumnoException;
+import sistema.excepciones.AsignaturaException;
+import sistema.excepciones.InscripcionException;
+import sistema.grafica.ventanas.VentanaListadoAlumnoDetallado;
+import sistema.grafica.ventanas.VentanaRegistroCalificacion;
+import sistema.logica.ICapaLogica;
+import sistema.logica.inscripciones.Inscripciones;
+import sistema.logica.valueObjects.VOAlumnoDetallado;
+import sistema.logica.valueObjects.VOInscripciones;
+
+public class ControladorRegistroCalificacion{
+
+	private ICapaLogica interfazFachada;
+	private VentanaRegistroCalificacion ventRegCalif;
+	
+	public ControladorRegistroCalificacion(VentanaRegistroCalificacion vRegCal) {
+		ventRegCalif = vRegCal;
+		
+		try
+		{
+			//Intento conectarme
+			Properties p=new Properties();
+			String nomArch="config/config.properties";
+			p.load(new FileInputStream(nomArch));
+			String ip=p.getProperty("ipServidor");
+			String puerto=p.getProperty("puertoServidor");
+			String ruta="//"+ip+":"+puerto+"/fachada";
+			
+			//Voy a buscar el objeto remoto
+			interfazFachada = (ICapaLogica) Naming.lookup(ruta);
+			
+		}
+		catch(MalformedURLException mEx)
+		{
+			mEx.printStackTrace();			
+		}
+		catch(IOException ioEx)
+		{
+			ioEx.printStackTrace();			
+		}
+		catch(NotBoundException nobEx)
+		{
+			nobEx.printStackTrace();
+		}	
+	}
+	
+	public void registrarCalificacion (long ced, int codIns, int nota) throws AlumnoException, InscripcionException, RemoteException {
+		
+		try {
+			interfazFachada.registrarResultadoAsignatura(ced, codIns, nota);
+			ventRegCalif.mostrarResultado("Calificacion ingresada!");
+		}
+		catch (InscripcionException insEx) {
+			ventRegCalif.mostrarError(insEx.darMensaje());
+		}
+		catch (AlumnoException aluEx) {
+			ventRegCalif.mostrarError(aluEx.darMensaje());
+		}
+		catch (RemoteException remEx) {
+			ventRegCalif.mostrarError(remEx.toString());
+		}
+	}
+	
+	
+	
+
+	
+}
